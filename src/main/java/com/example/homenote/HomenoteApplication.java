afe788f535f6fc1d22b96b605ef7cbcce12293f3
XuZeyun <2723900@qq.com>
2021-11-15 21:13:40 +0800

    构建搭建

---
+++ src/main/java/com/example/homenote/HomenoteApplication.java
@@ -0,0 +1,14 @@
+package com.example.homenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// springboot 启动最基本的注解
+@SpringBootApplication
+public class HomenoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomenoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homenote/HomenoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomenoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
